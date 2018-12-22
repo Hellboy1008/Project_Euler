cbfe55f855b98541daf73775886412233320f069
Stephen Lee <srnlee771@gmail.com>
2018-12-22 22:02:29 +0800

    Complete question 16

---
+++ Problem_16.java
@@ -0,0 +1,24 @@
+
+//龍ONE
+
+import java.math.BigInteger;
+
+public class Problem_16 {
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        BigInteger value = BigInteger.ONE.add(BigInteger.ONE);
+        value = value.pow(1000);
+        String power = value.toString();
+        int answer = 0;
+        for (int i = 0; i < power.length(); i++) {
+            answer += Character.getNumericValue(power.charAt(i));
+        }
+        System.out.println(answer);
+        long finishTime = System.nanoTime();
+        double timeTaken = (double) (finishTime - startTime);
+        // System.out.println(answer);
+        System.out.println("Time Taken:" + timeTaken / 1000000000 + " seconds");
+    }
+
+}
