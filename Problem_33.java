1e9e32de8297bca790be0463246a59cbb0432a9f
Stephen Lee <srnlee771@gmail.com>
2019-02-04 23:51:34 -0800

    Initial commit

---
+++ Problem_33.java
@@ -0,0 +1,15 @@
+
+//龍ONE
+
+public class Problem_33 {
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        int answer = 0;
+        System.out.println("The product of these special terms is: " + answer);
+        long finishTime = System.nanoTime();
+        double timeTaken = (double) (finishTime - startTime);
+        System.out.println("Time Taken:" + timeTaken / 1000000000 + " seconds");
+    }
+
+}
