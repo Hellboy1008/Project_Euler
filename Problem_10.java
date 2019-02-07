fca5eb71afbfc0619194f7a5e39f5bc4cc08ef05
Stephen Lee <srnlee771@gmail.com>
2019-02-07 11:56:11 -0800

    Update prime number algorithm

--- Problem_10.java
+++ Problem_10.java
@@ -3,36 +3,32 @@
 
 public class Problem_10 {
 
+    private static boolean[] notPrime = new boolean[2000000 + 1];
+
     public static void main(String[] args) {
 
         long startTime = System.nanoTime();
-        long sum = 5; // count 2 and 3 beforehand
-        // Check for primes and add to total sum
-        for (long counter = 0; 6 * counter - 1 <= 2000000; counter++) {
-            if (checkPrime(6 * counter - 1) == true) {
-                sum += 6 * counter - 1;
-            }
-        }
-        for (long counter = 0; 6 * counter + 1 <= 2000000; counter++) {
-            if (checkPrime(6 * counter + 1) == true) {
-                sum += 6 * counter + 1;
-            }
-        }
+        long sum = findPrimeSum();
         long finishTime = System.nanoTime();
         double timeTaken = (double) (finishTime - startTime);
         System.out.println("The sum of all prime numbers below two million is: " + sum);
         System.out.println("Time Taken:" + timeTaken / 1000000000 + " seconds");
     }
 
-    private static boolean checkPrime(long check_number) {
-        for (int counter = 3; counter <= Math.sqrt(check_number); counter += 2) {
-            if (check_number % 5 == 0) {
-                return false;
-            }
-            if (check_number % counter == 0) {
-                return false;
+    private static long findPrimeSum() {
+        long primeSum = 0;
+        // set all non prime numbers to true
+        for (int index = 2; index < notPrime.length; index++) {
+            for (int counter = index * 2; counter < notPrime.length; counter += index) {
+                notPrime[counter] = true;
             }
         }
-        return true;
+        // sum all prime numbers
+        for (int index = 2; index < notPrime.length; index++) {
+            if (notPrime[index] == false) {
+                primeSum += (long) index;
+            }
+        }
+        return primeSum;
     }
 }
