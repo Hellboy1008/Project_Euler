45f798c84d103d7b560286db8383f56491b878e8
Stephen Lee <srnlee771@gmail.com>
2019-02-09 02:28:21 -0800

    More efficient approach to question

--- Problem_36.java
+++ Problem_36.java
@@ -1,50 +1,49 @@
 
 //龍ONE
 
-import java.math.BigInteger;
+import java.lang.StringBuilder;
+import java.util.ArrayList;
 
 public class Problem_36 {
 
+    private static ArrayList<Integer> palindromeNums = new ArrayList<Integer>();
+
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        BigInteger answer = sumOfDoubleBasePalindrome(1000000);
+        int answer = sumOfDoubleBasePalindrome(1000000);
         long finishTime = System.nanoTime();
         double timeTaken = (double) (finishTime - startTime);
         System.out.println("The sum of these special numbers is: " + answer);
         System.out.println("Time Taken:" + timeTaken / 1000000000 + " seconds");
     }
 
-    private static BigInteger sumOfDoubleBasePalindrome(int limit) {
-        BigInteger sum = BigInteger.ZERO;
+    private static void findPalindromes(int limit) {
         for (int count = 0; count < limit; count++) {
-            // check if the number itself is a palindrome
-            BigInteger a = new BigInteger("" + count);
-            if (checkPalindrome(a) == false) {
+            int originalNumber = count;
+            int reverse = 0;
+            if (originalNumber % 10 == 0) {
                 continue;
-
             }
-            // check if the binary is a palindrome
-            BigInteger binary = new BigInteger(Integer.toBinaryString(count));
-            if (checkPalindrome(binary) == true) {
-                sum = sum.add(a);
+            while (originalNumber >= 1) {
+                reverse = reverse * 10 + originalNumber % 10;
+                originalNumber /= 10;
+            }
+            if (count == reverse) {
+                palindromeNums.add(count);
+            }
+        }
+    }
+
+    private static int sumOfDoubleBasePalindrome(int limit) {
+        int sum = 0;
+        findPalindromes(limit);
+        for (int index = 0; index < palindromeNums.size(); index++) {
+            StringBuilder binary = new StringBuilder(Integer.toBinaryString(palindromeNums.get(index)));
+            if (binary.toString().equals(binary.reverse().toString())) {
+                sum += palindromeNums.get(index);
             }
         }
         return sum;
-    }
-
-    private static boolean checkPalindrome(BigInteger num) {
-        BigInteger originalNumber = num;
-        BigInteger reverse = BigInteger.ZERO;
-        BigInteger ten = new BigInteger("10");
-        while (originalNumber.compareTo(BigInteger.ONE) == 1 || originalNumber.equals(BigInteger.ONE)) {
-            reverse = reverse.multiply(ten).add(originalNumber.mod(ten));
-            originalNumber = originalNumber.divide(ten);
-        }
-        if (reverse.equals(num)) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }
