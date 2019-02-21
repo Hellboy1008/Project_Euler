2114b166e7dc71d09615dca39c8c2c0e59397de2
Stephen Lee <srnlee771@gmail.com>
2019-02-21 14:20:39 -0800

    Add comments and add parameter to method

--- Problem_44.java
+++ Problem_44.java
@@ -5,34 +5,35 @@ public class Problem_44 {
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        int answer = findPentagonalPair();
+        int answer = findPentagonalPair(3000); // guess and check
         long finishTime = System.nanoTime();
         double timeTaken = (double) (finishTime - startTime);
         System.out.println("The difference D is: " + answer);
         System.out.println("Time Taken:" + timeTaken / 1000000000 + " seconds");
     }
 
-    private static int findPentagonalPair() {
+    private static int findPentagonalPair(int limit) {
         int pentagonalOne;
         int pentagonalTwo;
         int difference;
         int sum;
         double differenceNVal;
         double sumNVal;
-        for (int countOne = 1; countOne < 3000; countOne++) {
-            for (int countTwo = countOne + 1; countTwo < 3000; countTwo++) {
+        // loop through pentagonal numbers 1 through 3000
+        for (int countOne = 1; countOne < limit; countOne++) {
+            for (int countTwo = countOne + 1; countTwo < limit; countTwo++) {
                 pentagonalOne = countOne * (3 * countOne - 1) / 2;
                 pentagonalTwo = countTwo * (3 * countTwo - 1) / 2;
                 difference = pentagonalTwo - pentagonalOne;
                 sum = pentagonalOne + pentagonalTwo;
                 differenceNVal = (Math.sqrt(24 * difference + 1) + 1) / 6;
                 sumNVal = (Math.sqrt(24 * sum + 1) + 1) / 6;
+                // check if the difference and sum are pentagonal numbers
                 if (differenceNVal == (int) differenceNVal && sumNVal == (int) sumNVal) {
-                   return difference;
+                    return difference;
                 }
             }
         }
         return 0;
     }
-
 }
