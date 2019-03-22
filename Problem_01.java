2ff514f789831af30a157d0cbecdc23ec26863a8
Stephen Lee <srnlee771@gmail.com>
2019-03-22 01:39:05 -0700

    remove magic numbers

--- Problem_01.java
+++ Problem_01.java
@@ -2,18 +2,22 @@
 //龍ONE
 
 public class Problem_01 {
+
+    private static final long TIME_CONVERSION = 1000000000;
+    private static final String TIME_TAKEN = "Time Taken:%s seconds";
+    private static final String ANSWER = "The sum of all multiples of 3 or 5 below 1000 is: ";
+    private static final int SUM_OF_MULTIPLES_THREE = 3 * (333 * (333 + 1)) / 2;
+    private static final int SUM_OF_MULTIPLES_FIVE = 5 * (199 * (199 + 1)) / 2;
+    private static final int SUM_OF_MULTIPLES_FIFTEEN = 15 * (66 * (66 + 1)) / 2;
+
     public static void main(String[] args) {
 
         long startTime = System.nanoTime();
         // Add sum of multiples of 3 and/or 5 under 1000
-        int totalSum = 0;
-        int sum_of_three = 3 * (333 * (333 + 1)) / 2;
-        int sum_of_five = 5 * (199 * (199 + 1)) / 2;
-        int sum_of_fifteen = 15 * (66 * (66 + 1)) / 2;
-        totalSum = sum_of_three + sum_of_five - sum_of_fifteen;
+        int totalSum = SUM_OF_MULTIPLES_THREE + SUM_OF_MULTIPLES_FIVE - SUM_OF_MULTIPLES_FIFTEEN;
         long finishTime = System.nanoTime();
         double timeTaken = (double) (finishTime - startTime);
-        System.out.println("The sum of all multiples of 3 or 5 below 1000 is: " + totalSum);
-        System.out.println("Time Taken:" + timeTaken / 1000000000 + " seconds");
+        System.out.println(ANSWER + totalSum);
+        System.out.printf(TIME_TAKEN, timeTaken / 1000000000);
     }
 }
