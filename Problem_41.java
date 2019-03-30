ac1846a9492875ebfb27179c701b11eb16842d7f
Stephen Lee <srnlee771@gmail.com>
2019-03-29 18:34:57 -0700

    remove magic numbers

--- Problem_41.java
+++ Problem_41.java
@@ -3,25 +3,31 @@
 
 public class Problem_41 {
 
+    private static final long TIME_CONVERSION = 1000000000;
+    private static final String TIME_TAKEN = "Time Taken:%s seconds";
+    private static final String ANSWER = "The largest n-digit pandigital prime is: ";
+    private static final int UPPER_BOUND = 10000000;
+    private static final int SMALLEST_PRIME = 2;
+    private static final int TENS_DIGIT = 10;
     private static boolean[] notPrime;
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        int answer = findPandigitalPrime(10000000);
+        int answer = findPandigitalPrime(UPPER_BOUND);
         long finishTime = System.nanoTime();
         double timeTaken = (double) (finishTime - startTime);
-        System.out.println("The largest n-digit pandigital prime is: " + answer);
-        System.out.println("Time Taken:" + timeTaken / 1000000000 + " seconds");
+        System.out.println(ANSWER + answer);
+        System.out.printf(TIME_TAKEN, timeTaken / TIME_CONVERSION);
     }
 
     private static int findPandigitalPrime(int limit) {
         notPrime = new boolean[limit];
         // set all non prime numbers to true
-        for (int index = 2; index < notPrime.length; index++) {
-            if (index != 2 && index % 2 == 0) {
+        for (int index = SMALLEST_PRIME; index < notPrime.length; index++) {
+            if (index != SMALLEST_PRIME && index % SMALLEST_PRIME == 0) {
                 continue;
             }
-            for (int counter = index * 2; counter < notPrime.length; counter += index) {
+            for (int counter = index * SMALLEST_PRIME; counter < notPrime.length; counter += index) {
                 notPrime[counter] = true;
             }
         }
@@ -40,21 +46,21 @@ public class Problem_41 {
         int digitCount = 0;
         while (tempOne > 0) {
             digitCount++;
-            tempOne /= 10;
+            tempOne /= TENS_DIGIT;
         }
         // initialise array with false
-        boolean[] digits = new boolean[9];
+        boolean[] digits = new boolean[TENS_DIGIT - 1];
         int tempTwo = num;
         for (int count = 0; count < digitCount; count++) {
-            if (tempTwo % 10 == 0) {
+            if (tempTwo % TENS_DIGIT == 0) {
                 return false;
             }
-            if (digits[tempTwo % 10 - 1] == false) {
-                digits[tempTwo % 10 - 1] = true;
+            if (digits[tempTwo % TENS_DIGIT - 1] == false) {
+                digits[tempTwo % TENS_DIGIT - 1] = true;
             } else {
                 return false;
             }
-            tempTwo /= 10;
+            tempTwo /= TENS_DIGIT;
         }
         // check if it uses the digits from 1 to n exactly once
         for (int index = 0; index < digitCount; index++) {
