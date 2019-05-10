32efb2163758c2e6c45f67165453f3187304e715
Stephen Lee <srnlee771@gmail.com>
2019-05-10 14:47:37 -0700

    add comments and headers

--- Problem_01.java
+++ Problem_01.java
@@ -1,29 +1,82 @@
 
 /**
- * Created by: 龍ONE
+ * Created by: 龍ONE 
  * Date Created: Oct 12, 2017
- * Date Edited: May 8, 2019
+ * Date Edited: May 10, 2019
  * Purpose: Solution to Project Euler Problem 1
  */
 
+/**
+ * This class contains a method that calculates the sum of a multiple as well as
+ * the main method that executes the program.
+ */
 public class Problem_01 {
 
+    // limit for the problem
+    private static final int LIMIT = 1000;
+    // holds the multiple three
+    private static final int MULTIPLE_THREE = 3;
+    // holds the multiple five
+    private static final int MULTIPLE_FIVE = 5;
+
+    // conversion from nanoseconds to seconds
     private static final long TIME_CONVERSION = 1000000000;
-    private static final String TIME_TAKEN = "Time Taken:%s seconds";
+
+    // answer prompt
     private static final String ANSWER = "The sum of all multiples of 3 or 5 below 1000 is: ";
-    private static final int SUM_OF_MULTIPLES_THREE = 3 * (333 * (333 + 1)) / 2;
-    private static final int SUM_OF_MULTIPLES_FIVE = 5 * (199 * (199 + 1)) / 2;
-    private static final int SUM_OF_MULTIPLES_FIFTEEN = 15 * (66 * (66 + 1)) / 2;
+    // time take to solve the problem
+    private static final String TIME_TAKEN = "Time Taken:%s seconds";
 
+    /**
+     * The main method executes the solution and prints it alongside the time taken
+     * to solve the program.
+     * 
+     * @param args The arguments given to the main method
+     * @return None
+     */
     public static void main(String[] args) {
+        // total sum for the problem
+        int totalSum;
+        // end time of the program
+        long endTime;
+        // start time of the program
+        long startTime;
 
-        long startTime = System.nanoTime();
-        // Add sum of multiples of 3 and/or 5 under 1000 and subtract the multiples of
-        // fifteen
-        int totalSum = SUM_OF_MULTIPLES_THREE + SUM_OF_MULTIPLES_FIVE - SUM_OF_MULTIPLES_FIFTEEN;
-        long finishTime = System.nanoTime();
-        double timeTaken = (double) (finishTime - startTime);
+        startTime = System.nanoTime();
+        totalSum = sumOfMultiples(MULTIPLE_THREE, LIMIT) + sumOfMultiples(MULTIPLE_FIVE, LIMIT)
+                - sumOfMultiples(MULTIPLE_THREE * MULTIPLE_FIVE, LIMIT);
+        endTime = System.nanoTime();
+
+        // print answer and time taken
         System.out.println(ANSWER + totalSum);
-        System.out.printf(TIME_TAKEN, timeTaken / TIME_CONVERSION);
+        System.out.printf(TIME_TAKEN, (double) (endTime - startTime) / TIME_CONVERSION);
+    }
+
+    /**
+     * This method calculates the sum of all the multiples of a certain number below
+     * the limit. It uses the mathematical equation for the sum of first n natural
+     * numbers.
+     * 
+     * @param multiple The multiple being calculated
+     * @param limit    The limit for the sum
+     * @return The sum of all the multiples of a number below the limit
+     */
+    private static int sumOfMultiples(int multiple, int limit) {
+        // the maximum for the sum
+        int max;
+        // the sum
+        int sum = 0;
+
+        // calculate the maximum
+        if (limit % multiple == 0) {
+            max = limit / multiple - 1;
+        } else {
+            max = limit / multiple;
+        }
+
+        // calculate the sum
+        sum = multiple * (max * (max + 1)) / 2;
+
+        return sum;
     }
 }
