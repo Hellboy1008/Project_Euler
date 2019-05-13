4b362b73d9d360826a655b3a5ed7ccef162f1a95
Stephen Lee <srnlee771@gmail.com>
2019-05-12 18:55:39 -0700

    add file header and comments

--- Problem_02.java
+++ Problem_02.java
@@ -1,38 +1,81 @@
 
-//龍ONE
+/**
+ * Created by: 龍ONE 
+ * Date Created: Oct 12, 2017
+ * Date Edited: May 12, 2019
+ * Purpose: Solution to Project Euler Problem 2
+ */
 
+/**
+ * This class contains a method that calculates the sum of a even fibonacci
+ * numbers as well as the main method that executes the program.
+ */
 public class Problem_02 {
 
-    private static final long TIME_CONVERSION = 1000000000;
-    private static final String TIME_TAKEN = "Time Taken:%s seconds";
-    private static final String ANSWER = "The sum of even fibonacci numbers under 4 million is: ";
-    private static final int SUM_OF_FIRST_THREE_FIBONACCI = 2;
+    // multiplier for the equation used to calculate the sum
     private static final int EVEN_FIBONACCI_MULTIPLIER = 4;
+    // sum of the first three fibonacci numbers (0,1,1)
+    private static final int SUM_OF_FIRST_THREE_FIBONACCI = 2;
+    // upper limit for the problem
     private static final int UPPER_BOUND = 4000000;
 
-    public static void main(String[] args) {
+    // conversion from nanoseconds to seconds
+    private static final long TIME_CONVERSION = 1000000000;
 
-        long startTime = System.nanoTime();
-        // Declare variables used for calculations (assume 2 is already part of sum)
-        int previous_fibonacci_number = 0;
-        int even_fibonacci_number = SUM_OF_FIRST_THREE_FIBONACCI;
+    // answer prompt
+    private static final String ANSWER = "The sum of even fibonacci numbers under 4 million is: ";
+    // time take to solve the problem
+    private static final String TIME_TAKEN = "Time Taken:%s seconds";
+
+    /**
+     * The main method executes the solution and prints it alongside the time taken
+     * to solve the program.
+     * 
+     * @param args The arguments given to the main method
+     * @return None
+     */
+    public static void main(String[] args) {
+        // sum for the problem
+        int sum;
+        // end time of the program
+        long endTime;
+        // start time of the program
+        long startTime;
+
+        startTime = System.nanoTime();
+        sum = calculateEvenFibonaciiSum(UPPER_BOUND);
+        endTime = System.nanoTime();
+
+        // print answer and time taken
+        System.out.println(ANSWER + sum);
+        System.out.printf(TIME_TAKEN, (double) (endTime - startTime) / TIME_CONVERSION);
+    }
+
+    /**
+     * This methods calculates the sum of even fibonacci numbers using the idea that
+     * every third fibonacci numbers is even starting from 2.
+     * 
+     * @param limit The limit of the sum
+     * @return The sum of all even fibonacci numbers smaller than limit
+     */
+    private static int calculateEvenFibonaciiSum(int limit) {
+        // holds the future fibonacci number
         int calculated_fibonacci = 0;
-        int totalSum = SUM_OF_FIRST_THREE_FIBONACCI;
-        boolean endLoop = false;
+        // holds the current fibonacci number
+        int even_fibonacci_number = SUM_OF_FIRST_THREE_FIBONACCI;
+        // holds the previous fibonacci number
+        int previous_fibonacci_number = 0;
+        // holds the sum of the fibonacci numbers
+        int sum = SUM_OF_FIRST_THREE_FIBONACCI;
+
         // Calculate the sum of the even fibonacci numbers
-        while (endLoop == false) {
+        while (calculated_fibonacci + calculated_fibonacci < limit) {
             calculated_fibonacci = EVEN_FIBONACCI_MULTIPLIER * even_fibonacci_number + previous_fibonacci_number;
-            if (calculated_fibonacci < UPPER_BOUND) {
-                totalSum += calculated_fibonacci;
-                previous_fibonacci_number = even_fibonacci_number;
-                even_fibonacci_number = calculated_fibonacci;
-            } else {
-                endLoop = true;
-            }
+            sum += calculated_fibonacci;
+            previous_fibonacci_number = even_fibonacci_number;
+            even_fibonacci_number = calculated_fibonacci;
         }
-        long finishTime = System.nanoTime();
-        double timeTaken = (double) (finishTime - startTime);
-        System.out.println(ANSWER + totalSum);
-        System.out.printf(TIME_TAKEN, timeTaken / TIME_CONVERSION);
+
+        return sum;
     }
 }
--- Problem_03.java
+++ Problem_03.java
@@ -1,36 +1,80 @@
 
-//龍ONE
+/**
+ * Created by: 龍ONE 
+ * Date Created: Oct 12, 2017
+ * Date Edited: May 12, 2019
+ * Purpose: Solution to Project Euler Problem 3
+ */
 
+/**
+ * This class contains a method that calculates the all the prime factors of a
+ * number as well as the main method that executes the program.
+ */
 public class Problem_03 {
 
-    private static final long TIME_CONVERSION = 1000000000;
-    private static final String TIME_TAKEN = "Time Taken:%s seconds";
-    private static final String ANSWER = "The largest prime factor is: ";
+    // smallest prime factor of a number
     private static final int SMALLEST_FACTOR = 2;
-    private static final String NUMBER = "600851475143";
 
+    // input for the problem
+    private static final long INPUT = 600851475143L;
+    // conversion from nanoseconds to seconds
+    private static final long TIME_CONVERSION = 1000000000;
+
+    // answer prompt
+    private static final String ANSWER = "The largest prime factor is: ";
+    // time take to solve the problem
+    private static final String TIME_TAKEN = "Time Taken:%s seconds";
+
+    /**
+     * The main method executes the solution and prints it alongside the time taken
+     * to solve the program.
+     * 
+     * @param args The arguments given to the main method
+     * @return None
+     */
     public static void main(String[] args) {
+        // end time of the program
+        long endTime;
+        // largest factor for the input
+        long largestFactor;
+        // start time of the program
+        long startTime;
 
-        long startTime = System.nanoTime();
-        long target_number = Long.parseLong(NUMBER);
-        // Use prime factorization to find largest prime factor
-        for (int factor = SMALLEST_FACTOR; factor < Math.sqrt(target_number); factor++) {
-            if (checkPrime(factor) == true) {
-                if (target_number % factor == 0) {
-                    target_number = target_number / factor;
-                }
-            }
-        }
+        startTime = System.nanoTime();
+        largestFactor = findLargestPrimeFactor(INPUT);
+        endTime = System.nanoTime();
 
-        long finishTime = System.nanoTime();
-        double timeTaken = (double) (finishTime - startTime);
-        System.out.println(ANSWER + target_number);
-        System.out.printf(TIME_TAKEN, timeTaken / TIME_CONVERSION);
+        // print answer and time taken
+        System.out.println(ANSWER + largestFactor);
+        System.out.printf(TIME_TAKEN, (double) (endTime - startTime) / TIME_CONVERSION);
     }
 
-    private static boolean checkPrime(long check_number) {
-        for (int counter = SMALLEST_FACTOR; counter <= Math.sqrt(check_number); counter++) {
-            if (check_number % counter == 0) {
+    /**
+     * This methods find the largest prime factor of a number.
+     * 
+     * @param target The number in question
+     * @return The largest prime factor of the number
+     */
+    private static long findLargestPrimeFactor(long target) {
+        // Use prime factorization to find largest prime factor
+        for (int factor = SMALLEST_FACTOR; factor < Math.sqrt(target); factor++) {
+            if (checkPrime(factor) == true && target % factor == 0) {
+                target = target / factor;
+            }
+        }
+        return target;
+    }
+
+    /**
+     * This method determines whether a number is prime.
+     * 
+     * @param number The number in question
+     * @return True if the number is a prime number, false if not
+     */
+    private static boolean checkPrime(long number) {
+        // determine whether the number is divisible by any number other than itself
+        for (int counter = SMALLEST_FACTOR; counter <= Math.sqrt(number); counter++) {
+            if (number % counter == 0 && number != counter) {
                 return false;
             }
         }
