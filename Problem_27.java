33a2fa736a0a3cddd807260307f6eb5b866fcde2
Stephen Lee <srlee@ucsd.edu>
2020-07-04 22:39:13 +0800

    add file headers and comments

--- Problem_27.java
+++ Problem_27.java
@@ -1,63 +1,133 @@
 
-//龍ONE
+/**
+ * Created by: 龍ONE 
+ * Date Created: January 31, 2019
+ * Date Edited: July 4, 2020
+ * Purpose: Solution to Project Euler Problem 27
+ */
 
 import java.util.ArrayList;
 
+/**
+ * This class contains a method that calculates the product of the coefficients
+ * a and b, where |a| < l and |b| <= l for the quadratic expression n^2 + an + b
+ * that produces the maximum number of primes for consecutive values of n,
+ * starting with n = 0. The main method executes the program.
+ */
 public class Problem_27 {
 
-    private static final long TIME_CONVERSION = 1000000000;
-    private static final String TIME_TAKEN = "Time Taken:%s seconds";
-    private static final String ANSWER = "The product of the coefficients is ";
-    private static final int UPPER_BOUND = 1000;
+    // the smallest prime number
     private static final int SMALLEST_PRIME = 2;
+    // upper bound for the problem
+    private static final int UPPER_BOUND = 1000;
 
+    // conversion from nanoseconds to seconds
+    private static final long TIME_CONVERSION = 1000000000;
+
+    // answer prompt
+    private static final String ANSWER = "The product of the coefficients where |a| < " + UPPER_BOUND + " and |b| <= "
+            + UPPER_BOUND + " is: ";
+    // time take to solve the problem
+    private static final String TIME_TAKEN = "Time Taken:%s seconds";
+
+    /**
+     * The main method executes the solution and prints it alongside the time taken
+     * to solve the program.
+     * 
+     * @param args The arguments given to the main method
+     * @return None
+     */
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
-        int answer = productOfCoefficients(UPPER_BOUND, UPPER_BOUND);
-        long finishTime = System.nanoTime();
-        double timeTaken = (double) (finishTime - startTime);
-        System.out.println(ANSWER + answer);
-        System.out.printf(TIME_TAKEN, timeTaken / TIME_CONVERSION);
+        // solution for the problem
+        int solution;
+        // end time of the program
+        long endTime;
+        // start time of the program
+        long startTime;
+
+        startTime = System.nanoTime();
+        solution = productOfCoefficients(UPPER_BOUND);
+        endTime = System.nanoTime();
+
+        // print answer and time taken
+        System.out.println(ANSWER + solution);
+        System.out.printf(TIME_TAKEN, (double) (endTime - startTime) / TIME_CONVERSION);
     }
 
-    private static int productOfCoefficients(int max_value_one, int max_value_two) {
-        ArrayList<Integer> b_values = new ArrayList<Integer>();
-        int largestNumOfPrimes = 0;
+    /**
+     * Calculates the product of the coefficients a and b, where |a| < l and |b| <=
+     * l for the quadratic expression n^2 + an + b that produces the maximum number
+     * of primes for consecutive values of n, starting with n = 0.
+     * 
+     * @param limit The upper limit for the coefficients
+     * @return The product of the coefficients that satisfy the requirement
+     */
+    private static int productOfCoefficients(int limit) {
+        // keeps track of the value of a with longest consecutive prime numbers
         int largest_a_value = 0;
+        // keeps track of the value of b with longest consecutive prime numbers
         int largest_b_value = 0;
-        // values for b must be prime
-        for (int counter = SMALLEST_PRIME; counter <= max_value_two; counter++) {
+        // the length of the largest number of consecutive primes
+        int largestNumOfPrimes = 0;
+        // keeps track of the value of n
+        int n_value;
+        // keeps track of the number of primes for a pair of a and b
+        int numberOfPrimes = 0;
+        // ArrayList that contains all possible values for b
+        ArrayList<Integer> b_values = new ArrayList<Integer>();
+
+        // loops through all possible values of b
+        for (int counter = SMALLEST_PRIME; counter <= limit; counter++) {
+            // b must be a prime, so only add prime numbers to ArrayList
             if (checkPrime(counter) == true) {
                 b_values.add(counter);
             }
         }
-        // calculate largest coefficients
-        int n_value = 0;
-        int numberOfPrimes = 0;
-        for (int counter = -max_value_one; counter < max_value_one; counter++) {
+
+        // loops through all possible values of a
+        for (int counter = -limit; counter < limit; counter++) {
+            // loops through all possible values of b
             for (int index = 0; index < b_values.size(); index++) {
+                n_value = 0;
+                numberOfPrimes = 0;
+                // count how many consecutive primes are produced
                 while (checkPrime(Math.abs(n_value * n_value + counter * n_value + b_values.get(index))) == true) {
                     n_value++;
                     numberOfPrimes++;
                 }
-                n_value = 0;
+                // if the number of consecutive primes is greater than the current longest
+                // length
                 if (numberOfPrimes > largestNumOfPrimes) {
                     largestNumOfPrimes = numberOfPrimes;
                     largest_a_value = counter;
                     largest_b_value = b_values.get(index);
                 }
-                numberOfPrimes = 0;
             }
         }
+
         return largest_a_value * largest_b_value;
     }
 
-    private static boolean checkPrime(int check_number) {
-        for (int counter = SMALLEST_PRIME; counter <= Math.sqrt(check_number); counter++) {
-            if (check_number % counter == 0) {
+    /**
+     * Returns whether a number is a prime number.
+     * 
+     * @param num The number to be checked
+     * @return True if the number is a prime number, false otherwise
+     */
+    private static boolean checkPrime(int num) {
+        // check if the number is 0 or 1
+        if (num == 0 || num == 1) {
+            return false;
+        }
+
+        // loops from 2 to the square root of the number to find potential factors
+        for (int counter = SMALLEST_PRIME; counter <= Math.sqrt(num); counter++) {
+            // checks if the number is divisible
+            if (num % counter == 0) {
                 return false;
             }
         }
+
         return true;
     }
 
--- Problem_28.java
+++ Problem_28.java
@@ -1,48 +1,90 @@
 
-//龍ONE
+/**
+ * Created by: 龍ONE 
+ * Date Created: January 31, 2019
+ * Date Edited: July 4, 2020
+ * Purpose: Solution to Project Euler Problem 28
+ */
 
-import java.util.ArrayList;
-
+/**
+ * This class contains a method that calculates sum of the numbers on the
+ * diagonals in a n x n spiral. The main method executes the program.
+ */
 public class Problem_28 {
 
-    private static final long TIME_CONVERSION = 1000000000;
-    private static final String TIME_TAKEN = "Time Taken:%s seconds";
-    private static final String ANSWER = "The sum of the diagonal is ";
-    private static final int SPIRAL_SIZE = 1001;
-    private static final int SMALLEST_SQUARE = 3;
-    private static final int SIZE_INCREMENT = 2;
+    // the square value for length 2 diagonal
+    private static final int LENGTH_TWO_DIAGONAL_SQUARE = 3;
+    // multiplier for the equations used to solve problem
     private static final int MULTIPLIER_FOUR = 4;
+    // the increment for the square value every time the length of the diagonal
+    // increases
+    private static final int SIZE_INCREMENT = 2;
+    // the spiral size for the problem
+    private static final int SPIRAL_SIZE = 1001;
 
+    // conversion from nanoseconds to seconds
+    private static final long TIME_CONVERSION = 1000000000;
+
+    // answer prompt
+    private static final String ANSWER = "The sum of the diagonal in the " + SPIRAL_SIZE + " x " + SPIRAL_SIZE
+            + " is: ";
+    // time take to solve the problem
+    private static final String TIME_TAKEN = "Time Taken:%s seconds";
+
+    /**
+     * The main method executes the solution and prints it alongside the time taken
+     * to solve the program.
+     * 
+     * @param args The arguments given to the main method
+     * @return None
+     */
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
-        int answer = calculateDiagonalSum(SPIRAL_SIZE);
-        long finishTime = System.nanoTime();
-        double timeTaken = (double) (finishTime - startTime);
-        System.out.println(ANSWER + answer);
-        System.out.printf(TIME_TAKEN, timeTaken / TIME_CONVERSION);
+        // solution for the problem
+        int solution;
+        // end time of the program
+        long endTime;
+        // start time of the program
+        long startTime;
+
+        startTime = System.nanoTime();
+        solution = calculateDiagonalSum(SPIRAL_SIZE);
+        endTime = System.nanoTime();
+
+        // print answer and time taken
+        System.out.println(ANSWER + solution);
+        System.out.printf(TIME_TAKEN, (double) (endTime - startTime) / TIME_CONVERSION);
     }
 
+    /**
+     * Calculates sum of the numbers on the diagonals in a n x n spiral, where n is
+     * the size.
+     * 
+     * @param size The size of the spiral
+     * @return The sum of the numbers on the diagonals
+     */
     private static int calculateDiagonalSum(int size) {
-        int lengthOfDiagonal = 1; // 1x1 matrix has length 1
-        for (int counter = SMALLEST_SQUARE; counter <= size; counter += SIZE_INCREMENT) {
+        // the sum of the diagonals (starting at 1 for the central number)
+        int diagonalSum = 1;
+        // holds the length of each diagonal
+        int lengthOfDiagonal = 1;
+
+        // calculates the length of the diagonals
+        for (int counter = LENGTH_TWO_DIAGONAL_SQUARE; counter <= size; counter += SIZE_INCREMENT) {
             lengthOfDiagonal++;
         }
-        int diagonalSum = 1; // account for the one in the middle
-        ArrayList<Integer> holder = new ArrayList<Integer>();
+
+        // calculates the sum of the diagonals for each corner
         for (int counter = 1; counter <= lengthOfDiagonal - 1; counter++) {
             // value of upper left diagonal
-            holder.add(MULTIPLIER_FOUR * counter * counter + SIZE_INCREMENT * counter + 1);
+            diagonalSum += MULTIPLIER_FOUR * counter * counter + SIZE_INCREMENT * counter + 1;
             // value of lower left diagonal
-            holder.add(MULTIPLIER_FOUR * counter * counter + 1);
+            diagonalSum += MULTIPLIER_FOUR * counter * counter + 1;
             // value of upper right diagonal
-            holder.add(MULTIPLIER_FOUR * counter * counter + MULTIPLIER_FOUR * counter + 1);
+            diagonalSum += MULTIPLIER_FOUR * counter * counter + MULTIPLIER_FOUR * counter + 1;
             // value of lower right diagonal
-            holder.add(MULTIPLIER_FOUR * counter * counter - SIZE_INCREMENT * counter + 1);
+            diagonalSum += MULTIPLIER_FOUR * counter * counter - SIZE_INCREMENT * counter + 1;
         }
-        // sum all values
-        for (int index = 0; index < holder.size(); index++) {
-            diagonalSum += holder.get(index);
-        }
+
         return diagonalSum;
     }
 
